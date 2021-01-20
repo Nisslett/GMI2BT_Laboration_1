8aa2c131916d7ba3694b06c321d976343866d837
Nisslett <>
2021-01-20 08:45:58 +0100

    formatering

--- modules.py
+++ modules.py
@@ -1,35 +1,39 @@
 # functions
 
-def generate_divisible_numbers(number1 ,number2):
-    number_list=[]
-    limit=1000
-    for counter in range(1,limit):
-        if counter%number1==0 and counter%number2==0:
+def generate_divisible_numbers(number1, number2):
+    number_list = []
+    limit = 1000
+    for counter in range(1, limit):
+        if counter % number1 == 0 and counter % number2 == 0:
             number_list.append(counter)
-    average=0
+    average = 0
     for item in number_list:
-        average+=item
-    average=average/len(number_list)
-    return { "number_list":number_list , "average":average}
+        average += item
+    average = average/len(number_list)
+    return {"number_list": number_list, "average": average}
+
 
 def guessing_number():
-    limit_upper=100
-    limit_lower=1
+    limit_upper = 100
+    limit_lower = 1
     from random import randint
-    rand_number=randint(limit_lower,limit_upper)
+    rand_number = randint(limit_lower, limit_upper)
     while True:
-        try_input=input(f"Guess a number betwenne {limit_lower} to {limit_upper} :")
+        try_input = input(
+            f"Guess a number betwenne {limit_lower} to {limit_upper} :")
         if not try_input.isdigit():
             print("Input is not a number!")
             continue
         else:
-            try_input=int(try_input)
-        if try_input==rand_number:
-            print(f"WOOHOO!!! Your guess of {try_input} was correct. Congratualtions!")
+            try_input = int(try_input)
+        if try_input == rand_number:
+            print(
+                f"WOOHOO!!! Your guess of {try_input} was correct. Congratualtions!")
             break
         else:
-            if rand_number>try_input:
-                hint="larger"
+            if rand_number > try_input:
+                hint = "larger"
             else:
-                hint="smaller"
-            print(f"Your guess was incorrect , the nubmer your looking for is {hint}.")
+                hint = "smaller"
+            print(
+                f"Your guess was incorrect , the nubmer your looking for is {hint}.")
--- program.py
+++ program.py
@@ -1,33 +1,35 @@
-#UI (User Interface)
+# UI (User Interface)
 import modules
+
 
 def input_number(text_value):
     while True:
-        input_value=input(text_value)
+        input_value = input(text_value)
         if input_value.isdigit():
             return int(input_value)
         print("Invalid input,input not a number!")
 
+
 def divisable_numbers():
     print("Enter two numbers to find common divisable numbers up to 1000.")
-    number1=input_number("Input the first nubmer:")
-    number2=input_number("Input the second nubmer:")
-    print(modules.generate_divisible_numbers(number1,number2))
+    number1 = input_number("Input the first nubmer:")
+    number2 = input_number("Input the second nubmer:")
+    print(modules.generate_divisible_numbers(number1, number2))
+
 
 def menu():
     while True:
         print("1. Generate Divisable Numbers\n2. Guessing Game\n3. Exit")
-        choice=input_number("Input choice:")
-        if choice==1:
+        choice = input_number("Input choice:")
+        if choice == 1:
             divisable_numbers()
-        elif choice==2:
+        elif choice == 2:
             modules.guessing_number()
-        elif choice==3:
+        elif choice == 3:
             print("Exiting Menu.")
             return
         else:
             print("Invalid option! Try again.")
 
+
 menu()
-
-
